bdd1e9e027635013935d4acdd32940fa390d46e3
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-06-01 17:18:34 +0530

    Update NullPointerExceptionExample.java

--- NullPointerExceptionExample.java
+++ NullPointerExceptionExample.java
@@ -1,10 +1,11 @@
 class NullPointerExceptionExample{
 
-	//
+	// Here we made the str variable as null
 	static String str = null;
 
 	public static void main(String[] args) {
-
+		//here we are trying to access the length of the str variable which is assigned to null 
+		// when we try to access the null associated variables, then it will throw NullPointerException.
 		System.out.println(str.length());
 	}
 }
